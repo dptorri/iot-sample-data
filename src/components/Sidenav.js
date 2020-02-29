df0b804932da3654932cb3c55b19e69ae7ac434d
Daniel Palacios <daniel.palacios@prodyna.com>
2020-02-29 13:24:21 +0100

    header and sidenav components added

---
+++ src/components/Sidenav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sidenav.css';
+
+function Sidenav() {
+  return (
+    <div className="sidenav">
+        <span>Sidenav content</span>
+    </div>
+  );
+}
+
+export default Sidenav;
